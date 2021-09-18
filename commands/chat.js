7aacb9e3ca16569437743e75338156cb7e0f7659
hoang vo <hoangdk0911@gmail.com>
2021-09-18 19:57:30 +0700

    update secret chat

--- commands/chat.js
+++ commands/chat.js
@@ -3,14 +3,16 @@ const axios = require("axios");
 const hardMessage = {
     F4T: 'Phắc boi có tiếng, làm gì vậy mấy thanh niên?',
     Hoàng: 'thông minh nhất channel'
-
 }
+const secrets = ['Hoàng', 'hoang'];
 
 exports.run = async (client, message) => {
     try {
         let text = message.content.replace('!chat ', '');
         let result;
-        if (hardMessage[text]) {
+        if (secrets.some(e => text.toLowerCase().includes(e.toLowerCase()))) {
+            result = 'Bí mật quốc gia';
+        } else if (hardMessage[text]) {
             result = hardMessage[text];
         } else {
             let response = await axios.get(`https://api.simsimi.net/v2/`, {
